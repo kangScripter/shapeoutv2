b32ab7752a4fecef1508358f5c4a64c3479e4b6f
teja9949 <67798221+teja9949@users.noreply.github.com>
2021-07-07 11:02:22 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -32,7 +32,7 @@ async def storefile(c, m):
     text += f"__👁 User Name:__ @{m.from_user.username}\n\n" if m.from_user.username else ""
     text += f"__👤 User Id:__ `{m.from_user.id}`\n\n"
     text += f"__💬 DC ID:__ {m.from_user.dc_id}\n\n" if m.from_user.dc_id else ""
-    text += f"__📎 Copy Url:__ url ""
+    text += f"__📎 Copy Url:__ "url ""
     # if databacase channel exist forwarding message to channel
     if DB_CHANNEL_ID:
         msg = await m.copy(int(DB_CHANNEL_ID))
